f442cfca096d00105393db892fc1978dba11ee15
Cidezog <enzo09100@gmail.com>
2016-11-03 10:35:35 +0100

    Ajout test

--- src/main/java/efra/Item.java
+++ src/main/java/efra/Item.java
@@ -4,17 +4,17 @@ package efra;
  * Created by 21204031 on 20/10/2016.
  */
 public class Item {
-    private int valeur;
+    private Object value;
 
-    public Item (int valeur) {
-        this.valeur = valeur;
+    Item (Object value) {
+        setValue(value);
     }
 
-    public int getValeur () {
-        return valeur;
+    public Object getValue() {
+        return value;
     }
 
-    public void setValeur (int valeur) {
-        this.valeur = valeur;
+    public void setValue (Object value) {
+        this.value = value;
     }
 }
